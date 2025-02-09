2821f3ce6289974e108186e2aa103a93acd4c70e
raghavarupesh1 <raghavarupesh@gmail.com>
2025-02-09 12:14:24 +0000

    added code

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-       source = "hasicorp/azurerm"
+       source = "hashicorp/azurerm"
        version = "4.17.0"
     }
   }
@@ -12,21 +12,16 @@ provider "azurerm" {
   client_id = "ea729ccf-e7a8-4e48-b161-72514c0f6875"
   client_secret ="P~f8Q~4tVt04t7Zq~jTzo4tIWSvof8gTw2aUTbWF"
   tenant_id = "4cde2737-8a7c-435a-babe-cbf48917b024"
+  features {}
 }
 
 data "azurerm_resource_group" "rg" {
   name = "Srini_RG"
-  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name = "myvnet1"
-  location = data.azurerm_resource_group.location
-  resource_group_name = data.azurerm_resource_group.name
+  location = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   address_space = ["10.0.0.0/16"]
-
-
-  subnet = {
-    address_prefixes = ["10.0.1.0/24"]
-  }
 }
